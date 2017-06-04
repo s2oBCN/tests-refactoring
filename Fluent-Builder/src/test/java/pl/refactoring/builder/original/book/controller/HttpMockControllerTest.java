42fd4f6eaa325f000ea2fb99329f599208ef80df
Włodek Krakowski <wlodzimierz.krakowski@onet.pl>
2017-06-04 11:43:23 +0200

    Trial.

---
+++ Fluent-Builder/src/test/java/pl/refactoring/builder/original/book/controller/HttpMockControllerTest.java
@@ -0,0 +1,29 @@
+package pl.refactoring.builder.original.book.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+/**
+ * Created by madzia1 on 2017-06-03.
+ */
+public class HttpMockControllerTest {
+    protected MockMvc mvc;
+
+    protected VerificationOptions doGet(String urlTemplate) throws Exception {
+        ResultActions resultActions = mvc.perform(get(urlTemplate)
+                .accept(MediaType.APPLICATION_JSON));
+
+        return new VerificationOptions(resultActions);
+    }
+
+    protected VerificationOptions doDelete(String urlTemplate) throws Exception {
+        ResultActions resultActions = mvc.perform(delete(urlTemplate)
+                .accept(MediaType.APPLICATION_JSON));
+
+        return new VerificationOptions(resultActions);
+    }
+}
