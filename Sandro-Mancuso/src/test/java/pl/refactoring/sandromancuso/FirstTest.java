89c7cee983a3e9cae691997babf2ea89228fa1af
Wlodzimierz Krakowski <w.krakowski@ocado.com>
2017-06-21 11:40:52 +0200

    Init

---
+++ Sandro-Mancuso/src/test/java/pl/refactoring/sandromancuso/FirstTest.java
@@ -0,0 +1,14 @@
+package pl.refactoring.sandromancuso;
+
+import org.junit.Test;
+
+/**
+ * Created by w.krakowski on 6/21/2017.
+ */
+public class FirstTest {
+    @Test
+    public void name() throws Exception {
+
+
+    }
+}
