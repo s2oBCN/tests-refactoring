619bad4aed68a00cfd74ea9d1cd0cc353d878e20
Wlodzimierz Krakowski <w.krakowski@ocado.com>
2017-06-21 11:45:03 +0200

    Iteration 1

---
+++ Sandro-Mancuso/src/main/java/pl/refactoring/sandromansuso/Board.java
@@ -0,0 +1,8 @@
+package pl.refactoring.sandromansuso;
+
+/**
+ * Created by w.krakowski on 6/21/2017.
+ */
+public class Board {
+
+}
--- Sandro-Mancuso/src/test/java/pl/refactoring/sandromancuso/FirstTest.java
+++ Sandro-Mancuso/src/test/java/pl/refactoring/sandromancuso/FirstTest.java
@@ -1,14 +1,18 @@
 package pl.refactoring.sandromancuso;
 
 import org.junit.Test;
+import pl.refactoring.sandromansuso.Board;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by w.krakowski on 6/21/2017.
  */
 public class FirstTest {
     @Test
-    public void name() throws Exception {
+    public void testBoardExist() throws Exception {
+        Board board = new Board();
 
-
+        assertTrue(true);
     }
 }
